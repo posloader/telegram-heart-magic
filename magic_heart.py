6b6f7b78127db7a98d434d6260442b4c98ae3948
posloader <63714264+posloader@users.noreply.github.com>
2022-02-22 00:41:07 +0300

    Update magic_heart.py

--- magic_heart.py
+++ magic_heart.py
@@ -9,7 +9,7 @@ API_HASH = '4037e9f957f6f17d461b0c288ffa50f1'
 
 HEART = '🤍'
 COLORED_HEARTS = ['💗', '💓', '💖', '💘', '❤️', '💞']
-MAGIC_PHRASES = ['magic']
+MAGIC_PHRASES = ['magic', '❤ magic', '❤magic']
 EDIT_DELAY = 0.1
 
 PARADE_MAP = '''
